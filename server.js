abeb1c1e4aeb36ea1186d7c88ac1b1d2f5798f1b
HichemHa <hichemhanafi012@gmail.com>
2024-08-31 21:16:15 +0200

    second com3

--- server.js
+++ server.js
@@ -5,46 +5,36 @@ const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
-
-app.use(cors());
-
 const io = socketIo(server, {
     cors: {
-        origin: "*", 
+        origin: "*",
         methods: ["GET", "POST"],
         credentials: true
     }
 });
+
+app.use(cors());
+app.use(express.json());
 
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
     socket.on('join', (room) => {
         socket.join(room);
-        console.log(`User ${socket.id} joined room ${room}`);
+        console.log(`${socket.id} joined room ${room}`);
 
-        const otherClients = io.sockets.adapter.rooms.get(room);
-        if (otherClients && otherClients.size > 1) {
-            otherClients.forEach((clientId) => {
-                if (clientId !== socket.id) {
-                    io.to(clientId).emit('new-peer', { peerId: socket.id });
-                }
-            });
-        }
+        // Notify other clients in the room
+        socket.to(room).emit('new-peer', { peerId: socket.id });
     });
 
-    socket.on('signal', (data) => {
-        const { to, signal } = data;
+    socket.on('signal', ({ to, signal }) => {
         io.to(to).emit('signal', { from: socket.id, signal });
-    });
-
-    socket.on('send-comment', (data) => {
-        const { roomId, comment } = data;
-        io.to(roomId).emit('receive-comment', { userId: socket.id, comment });
     });
 
     socket.on('disconnect', () => {
         console.log('User disconnected:', socket.id);
+        // Notify other clients in the room about the disconnection
+        io.emit('user-disconnected', socket.id);
     });
 });
 
