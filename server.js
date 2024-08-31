03bcf42704cceb4c420eaa7527d11a8662eb8515
HichemHa <hichemhanafi012@gmail.com>
2024-08-31 19:37:16 +0200

    second com

--- server.js
+++ server.js
@@ -3,49 +3,51 @@ const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
 
-const app = express(); // Create an Express app
-const server = http.createServer(app); // Create an HTTP server using the Express app
-
-// Example route
-app.use(cors());
-
+const app = express();
+const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "*", // Allow all origins
+        origin: "*", // Permettre toutes les origines
         methods: ["GET", "POST"],
         credentials: true
     }
 });
 
+app.use(cors());
 
-// Socket.IO connection handler
 io.on('connection', (socket) => {
     console.log('A user connected');
-    
+
+    socket.on('createRoom', (roomId) => {
+        socket.join(roomId);
+        console.log(`Room ${roomId} created by ${socket.id}`);
+    });
+
+    socket.on('joinRoom', (roomId) => {
+        socket.join(roomId);
+        console.log(`${socket.id} joined room ${roomId}`);
+
+        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || []);
+        const broadcaster = clients.find(clientId => clientId !== socket.id);
+
+        if (broadcaster) {
+            io.to(broadcaster).emit('newViewer', { peerId: socket.id });
+        }
+    });
+
+    socket.on('sendStream', ({ to, stream }) => {
+        io.to(to).emit('receiveStream', { peerId: socket.id, stream });
+    });
+
+    socket.on('comment', ({ roomId, message }) => {
+        io.to(roomId).emit('receiveComment', { from: socket.id, message });
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
-
-    // Your signaling and room handling logic here
-    socket.on('signal', (data) => {
-        const { to, signal } = data;
-        io.to(to).emit('signal', { from: socket.id, signal });
-    });
-
-    socket.on('join', (room) => {
-        socket.join(room);
-        const otherClients = io.sockets.adapter.rooms.get(room);
-        if (otherClients && otherClients.size > 1) {
-            otherClients.forEach((clientId) => {
-                if (clientId !== socket.id) {
-                    io.to(clientId).emit('new-peer', { peerId: socket.id });
-                }
-            });
-        }
-    });
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
